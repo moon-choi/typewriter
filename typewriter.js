703d23c6900a2001548299097bbb2b64ceed1015
Moon Choi <choimy1991@gmail.com>
2022-02-15 14:38:08 -0800

    adding a line change

--- typewriter.js
+++ typewriter.js
@@ -1,8 +1,8 @@
-const sentence = "hello there from lighthouse labs";
-const senteceWithNewLine = sentence + "\n"
+const sentence = "hello there from lighthouse labs"
+const sentenceN = sentence + "\n"
 
 let timer = 1000;
-for (const char of senteceWithNewLine) {
+for (const char of sentenceN) {
     setTimeout(() => {
         process.stdout.write(char);
     }, timer) // <= 1s delay to make it noticeable. Can dial it down later.
